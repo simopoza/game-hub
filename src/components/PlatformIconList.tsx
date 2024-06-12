bffa4afcaee05d62d9b152e9335f2f206502dbec
simopoza <mannahri@student.1337.ma>
2024-06-12 19:19:17 +0100

    Get Optimized Image

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -9,7 +9,7 @@ import {
 import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
-import { HStack, Icon } from "@chakra-ui/react";
+import { HStack, Icon, SimpleGrid } from "@chakra-ui/react";
 import { Platform } from "../hooks/useGames";
 import { IconType } from "react-icons";
 
@@ -31,11 +31,11 @@ const PlatformIconsList = ({ platforms }: Props) => {
   };
 
   return (
-    <HStack marginY={'10px'}>
+    <SimpleGrid columns={4} spacing={4} marginY="10px">
       {platforms.map((platform) => (
         <Icon as={iconMap[platform.slug]} color='gray.500' />
       ))}
-    </HStack>
+    </SimpleGrid>
   );
 };
 
