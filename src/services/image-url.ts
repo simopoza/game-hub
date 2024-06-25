0df23c7201385822f52989856b25ed2c7156f9a8
simopoza <mannahri@student.1337.ma>
2024-06-25 10:17:38 +0100

    Handle Games With No Image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,7 @@
+import no_image from '../assets/no_image_placeHolder.png'
 
 const getCroppedImageUrl = (url : string) => {
-	if (!url) return '';
+	if (!url) return no_image;
 	const target = 'media/';
 	const index = url.indexOf(target) + target.length;
 	return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
